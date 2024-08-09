686f166db6cfd21f8f9e58b4f86425a59e3b6f49
Jan Jerling <jan.jerling@gmail.com>
2024-08-09 21:07:03 +0200

    responsiveness

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links")
     const icon = document.querySelector(".hamburger-icon")
+    const menu = document.querySelector(".menu-links")
 
-    menu.classList.toggle("open");
     icon.classList.toggle("open");
+    menu.classList.toggle("open");
 }
 
 function scrollToTop(event) {
     event.preventDefault();
+
+    window.location.hash = '';
     window.scrollTo({
         top: 0,
         behavior: "smooth"
     });
+}
+
+function hamburgerToTop(event){
+    toggleMenu();
+    scrollToTop(event);
 }
